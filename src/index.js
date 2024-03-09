70f6c8564bd91cdc25d911a7cb1bcbfd3bf96304
trifles <132377611+unconsideredtrifles@users.noreply.github.com>
2024-03-10 00:30:59 +0630

    Prevent the empty city name from being submitted

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import getWeatherInfo from './api/weather-api';
 import  { setCurrentWeather, setWeatherForecasts } from './weather-util.js';
 
+
 function displayCityName(city) {
   const cityName = document.getElementsByClassName('city-name')[0];
   cityName.textContent = `${city} City's`;
@@ -24,3 +25,12 @@ searchBar.addEventListener('submit', (e) => {
   searchCityWeather(city);
   e.preventDefault();
 });
+
+
+const searchInput = document.getElementsByClassName('search-input')[0];
+searchInput.addEventListener('keydown', (e) => {
+  if (e.key === "Enter" && e.currentTarget.value.length === 0) {
+    e.preventDefault();
+  }
+});
+
