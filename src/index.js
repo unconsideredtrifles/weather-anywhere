3b1e6e39defe85e6d456687a6b4cbc7aef057bcb
trifles <132377611+unconsideredtrifles@users.noreply.github.com>
2024-03-10 00:03:09 +0630

    Make the city name reflect search result

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import getWeatherInfo from './api/weather-api';
 import  { setCurrentWeather, setWeatherForecasts } from './weather-util.js';
 
+function displayCityName(city) {
+  const cityName = document.getElementsByClassName('city-name')[0];
+  cityName.textContent = `${city} City's`;
+}
+
 
 function searchCityWeather(city) {
   getWeatherInfo(city).then((info) => {
+    displayCityName(city);
     setCurrentWeather(info.currentWeather);
     setWeatherForecasts(info.weatherForecasts);
   })/*.catch((error) => {
